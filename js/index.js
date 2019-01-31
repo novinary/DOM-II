0d3504a295dbb8cf826623c08cfbe7f0b3d7e086
novinary <novina.dev@gmail.com>
2019-01-31 01:42:54 +0000

    Created dblclick event listener for button 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 // Your code goes here
 const logoHeading = document.querySelector(".logo-heading");
 const introImg = document.querySelector("header img");
+const button1 = document.querySelector(".btn");
 
+//mouseover event listener
 logoHeading.addEventListener('mouseover', function(e) {
     introImg.style.display = "initial";
 })
+
+//dblclick event listener
+button1.addEventListener('dblclick', () => {
+    button1.textContent = "Ooo, fancy double click!";
+})
+
