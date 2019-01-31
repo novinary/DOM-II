4848f9d76be7f12051495366e62832d8694b2f53
novinary <novina.dev@gmail.com>
2019-01-31 01:46:49 +0000

    Created keypress event listener

--- js/index.js
+++ js/index.js
@@ -3,13 +3,18 @@ const logoHeading = document.querySelector(".logo-heading");
 const introImg = document.querySelector("header img");
 const button1 = document.querySelector(".btn");
 
-//mouseover event listener
+//mouseover 
 logoHeading.addEventListener('mouseover', function(e) {
     introImg.style.display = "initial";
 })
 
-//dblclick event listener
+//dblclick 
 button1.addEventListener('dblclick', () => {
     button1.textContent = "Ooo, fancy double click!";
+})
+
+//keypress
+window.addEventListener('keypress', () => {
+    alert("Hi, you've pressed a key!")
 })
 
